dd2e44997c9cbd63fad8b196e6317724e469d397
Love Bisaria <bisarialove@gmail.com>
2019-03-23 18:37:23 -0700

    changinf db ; adding sql script

---
+++ src/main/resources/sql/1.0.0.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`first_name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+	`last_name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+	`email` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+	`handle` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+	UNIQUE KEY `unique_email` (`email`) USING BTREE,
+    UNIQUE KEY `unique_handle` (`handle`) USING BTREE,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tweets` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(11) NOT NULL,
+	`tweet` VARCHAR(256) NOT NULL,
+	`tweet_data` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
